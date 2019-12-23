d8af71b53fca71ba6a6f35eabfcd2dfb3745a5f6
iLoveJia <914012706@qq.com>
2019-12-23 21:42:10 +0800

    init

---
+++ src/com/gunsmoke/Gunmoke.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2019 ucsmy.com, All rights reserved.
+ */
+package com.gunsmoke;
+
+/**
+ * @Description:
+ * @Author: Administrator
+ * @Created Date: 2019年12月23日
+ * @LastModifyDate:
+ * @LastModifyBy:
+ * @Version:
+ */
+public class Gunmoke {
+    private int age;
+}
